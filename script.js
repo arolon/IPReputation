0a86e54e8a6eb84a77c312d5039f9fd892ab408e
Felipe Rolon <afra.9622@gmail.com>
2024-03-29 01:23:47 -0400

    Fully customized with maps

--- script.js
+++ script.js
@@ -32,8 +32,111 @@ $form.addEventListener('submit', async (event) => {
   const ipInfo = await fetchIpReputation(value)
 
   if (ipInfo) {
-    $results.innerHTML = JSON.stringify(ipInfo, null, 2)
+    $results.innerHTML = `
+      <div class="grid">
+        <div>
+          <img id="flag" class="flag mx-auto d-block" src="${ipInfo.flag_image}" alt="Country Flag">
+        </div>
+        <div class="info">
+          <p><strong>Risk Level:</strong> <span id="riskLevel">${ipInfo.risk_level}</span></p>
+          <p><strong>Location:</strong> <span id="location">${ipInfo.city + ', ' + ipInfo.country + ', ' + ipInfo.continent}</span></p>
+          <p><strong>Organization:</strong> <span id="organization">${ipInfo.organization}</span></p>
+          <p><strong>Time Zone:</strong> <span id="timeZone">${ipInfo.time_zone}</span></p>
+          <p><strong>Currency:</strong> <span id="currency">${ipInfo.currency}</span></p>
+          <p><strong>Languages:</strong> <span id="languages">${ipInfo.languages[0].name}</span></p>
+        </div>
+      </div>
+      <p></p>
+      <article>
+      <header>Message:</header>
+      ${ipInfo.message}
+      </article>
+
+      <div class='grid'>
+        <table>
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-tooltip="Indicates whether the IP belongs to a data center">Is Datacenter</td>
+              <td>${ipInfo.is_datacenter?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP belongs to an Internet Service Provider">Is ISP</td>
+              <td>${ipInfo.is_isp?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP belongs to an educational institution">Is Educational</td>
+              <td>${ipInfo.is_educational?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP belongs to a military organization">Is Military</td>
+              <td>${ipInfo.is_military?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP belongs to a government entity">Is Government</td>
+              <td>${ipInfo.is_government?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP belongs to a business or commercial entity">Is Business</td>
+              <td>${ipInfo.is_business?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP is associated with a VPN or proxy service">Is VPN/Proxy</td>
+              <td>${ipInfo.is_vpn_proxy?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP is associated with the Tor network">Is Tor</td>
+              <td>${ipInfo.is_tor?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP is flagged as malicious">Is Malicious</td>
+              <td>${ipInfo.is_malicious?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP is associated with abusive behavior">Is Abusive</td>
+              <td>${ipInfo.is_abusive?"✔":''}</td>
+            </tr>
+            <tr>
+              <td data-tooltip="Indicates whether the IP is in a reserved or bogon range">Is Bogon</td>
+              <td>${ipInfo.is_bogon?"✔":''}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div id="map"></div>
+      </div>
+    
+
+      <details>
+        <summary role="button">Full response</summary>
+        <p><pre>${JSON.stringify(ipInfo, null, 2)}</pre></p>
+      </details>
+
+      
+    `
   }
+
+  let map;
+
+  async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+      center: { lat: ipInfo.latitude, lng: ipInfo.longitude },
+      zoom: 8,
+    });
+
+    const marker = new google.maps.Marker({
+      position: { lat: ipInfo.latitude, lng: ipInfo.longitude },
+      map: map,
+    });
+  }
+
+  initMap();
 
   $submit.removeAttribute('disabled')
   $submit.removeAttribute('aria-busy')
